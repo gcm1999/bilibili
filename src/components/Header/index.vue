ce2b555f14ce7093cbce53d027b8870e09ab3aeb
gcm <1453400328@qq.com>
2022-08-12 17:50:52 +0800

    header-img过渡动画

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -22,6 +22,22 @@ export default {
 .header {
   height: 180px;
   background: url(../../assets/header.png) 0 -36px no-repeat;
+  /* 过渡时间 */
+  transition-duration: 1s;
+  /* 过渡属性 */
+  transition-property: all;
+  /* 过渡延迟 */
+  transition-delay: 0.2s;
+
+  /* 过渡函数 
+                ease/ease-in-out        慢>快>慢
+                linear      匀速
+                ease-in     慢>快
+                ease-out    快>慢
+                step-start  直接变化
+                step-end    不变化过渡时间结束后直接变化
+            */
+  transition-timing-function: linear;
 }
 .bigImg {
   height: 360px;
