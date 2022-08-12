5fea69dd398b059eff659178dbce8e0e5b5d4799
gcm <1453400328@qq.com>
2022-08-12 17:45:01 +0800

    bilibili:初始化

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,14 +1,30 @@
 <template>
-  <div>
-    <img class="header-img" src="@/assets/header.png" alt="" />
+  <div class="header" :class="{ bigImg: show }" @click.self="bigImg">
     <h1>Header</h1>
   </div>
 </template>
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    bigImg() {
+      this.show = !this.show;
+    },
+  },
 };
 </script>
 <style scoped>
-
+.header {
+  height: 180px;
+  background: url(../../assets/header.png) 0 -36px no-repeat;
+}
+.bigImg {
+  height: 360px;
+  background: url(../../assets/header.png) no-repeat;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/reset.css";
+import router from "./router";
 
 import Header from "@/components/Header";
 
-createApp(App)
-  .component("Header", Header)
-  .mount("#app");
+createApp(App).component("Header", Header).use(router).mount("#app");
