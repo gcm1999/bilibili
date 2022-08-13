62476b9ef0d0447a4657d7f920025ebaa02254bd
gcm <1453400328@qq.com>
2022-08-13 23:12:15 +0800

    阿里矢量图标库iconfont使用

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,6 +1,63 @@
 <template>
   <div class="header" :class="{ bigImg: show }" @click.self="bigImg">
     <h1>Header</h1>
+    <div class="nav">
+      <div class="nav_left fl">
+        <ul class="iconfont">
+          <li class="icon-bilibili1">首页</li>
+          <li>番剧</li>
+          <li>直播</li>
+          <li>游戏中心</li>
+          <li>会员购</li>
+          <li>漫画</li>
+          <li>赛事</li>
+          <li>嘉年华</li>
+          <li class="icon-xiazai">下载客户端</li>
+        </ul>
+      </div>
+
+      
+
+      <div class="nav_right fr">
+        <ul class="iconfont">
+          <li>
+            <div>&#xe65d;</div>
+            大会员
+          </li>
+          <li>
+            <div>&#xe630;</div>
+            消息
+          </li>
+          <li>
+            <div>&#xeca7;</div>
+            动态
+          </li>
+          <li>
+            <div>&#xeca7;</div>
+            收藏
+          </li>
+          <li>
+            <div>&#xe608;</div>
+            历史
+          </li>
+          <li>
+            <div>&#xe6bc;</div>
+            创作中心
+          </li>
+          <li>投稿</li>
+        </ul>
+      </div>
+
+
+      <div class="nav_search">
+        <input type="text">
+      </div>
+    </div>
+
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
   </div>
 </template>
 <script>
@@ -19,9 +76,17 @@ export default {
 };
 </script>
 <style scoped>
+.fl {
+  float: left;
+}
+.fr {
+  float: right;
+}
 .header {
+  max-width: 1920px;
   height: 180px;
   background: url(../../assets/header.png) 0 -36px no-repeat;
+  /* cursor: pointer; */
   /* 过渡时间 */
   transition-duration: 1s;
   /* 过渡属性 */
@@ -37,10 +102,32 @@ export default {
                 step-start  直接变化
                 step-end    不变化过渡时间结束后直接变化
             */
-  transition-timing-function: linear;
+  transition-timing-function: ease-out;
 }
 .bigImg {
   height: 360px;
   background: url(../../assets/header.png) no-repeat;
+}
+.header .nav_left > ul {
+  text-align: center;
+  display: flex;
+  color: white;
+  font-size: 14px;
+}
+.header .nav_left > ul li {
+  margin: 8px;
+}
+.header .nav_right > ul {
+  text-align: center;
+  display: flex;
+  color: white;
+  font-size: 14px;
+}
+.header .nav_right > ul li {
+  margin: 0 8px;
+}
+
+.nav_search{
+  margin:0 auto;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import "./assets/reset.css";
 import router from "./router";
 
+import "./assets/iconfont/iconfont.css"
+import "./assets/iconfont/iconfont.js"
+
 import Header from "@/components/Header";
 
 createApp(App).component("Header", Header).use(router).mount("#app");
