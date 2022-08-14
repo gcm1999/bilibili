8bf05265919f5384c9822fb610639dddeb010edc
gcm <1453400328@qq.com>
2022-08-14 23:27:11 +0800

    搜索框

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="header" :class="{ bigImg: show }" @click.self="bigImg">
     <h1>Header</h1>
-    <div class="nav">
-      <div class="nav_left fl">
+    <div class="nav" v-show="!show">
+      <div class="nav-left">
         <ul class="iconfont">
           <li class="icon-bilibili1">首页</li>
           <li>番剧</li>
@@ -16,9 +16,15 @@
         </ul>
       </div>
 
-      
+      <div class="nav-search">
+        <div class="search-input">
+          <input type="text" placeholder="郭晨明成为世界首富" />
+          <img class="search-img" src="./images/sousuo.png" alt="" />
+        </div>
+        <div></div>
+      </div>
 
-      <div class="nav_right fr">
+      <div class="nav-right">
         <ul class="iconfont">
           <li>
             <div>&#xe65d;</div>
@@ -46,11 +52,6 @@
           </li>
           <li>投稿</li>
         </ul>
-      </div>
-
-
-      <div class="nav_search">
-        <input type="text">
       </div>
     </div>
 
@@ -84,7 +85,9 @@ export default {
 }
 .header {
   max-width: 1920px;
+  min-width: 1440px;
   height: 180px;
+  margin: 0 auto;
   background: url(../../assets/header.png) 0 -36px no-repeat;
   /* cursor: pointer; */
   /* 过渡时间 */
@@ -108,26 +111,106 @@ export default {
   height: 360px;
   background: url(../../assets/header.png) no-repeat;
 }
-.header .nav_left > ul {
+.header .nav {
+  display: flex;
+}
+.header .nav-left > ul {
   text-align: center;
   display: flex;
   color: white;
   font-size: 14px;
 }
-.header .nav_left > ul li {
+.header .nav-left > ul li {
+  cursor: pointer;
+  /* background-color: skyblue; */
+  height: 36px;
+  line-height: 36px;
   margin: 8px;
 }
-.header .nav_right > ul {
+.header .nav-left > ul li:hover {
+  animation: navmove 0.3s infinite;
+  animation-iteration-count: 1;
+  /* animation: 
+        name 
+        duration 
+        timing-function 
+        delay 
+        iteration-count     循环次数
+        direction 
+        fill-mode; */
+}
+@keyframes navmove {
+  0% {
+    line-height: 36px;
+  }
+  25% {
+    line-height: 24px;
+  }
+  50% {
+    line-height: 14px;
+  }
+  75% {
+    line-height: 24px;
+  }
+  100% {
+    line-height: 36px;
+  }
+}
+.header .nav-right > ul {
   text-align: center;
   display: flex;
   color: white;
   font-size: 14px;
 }
-.header .nav_right > ul li {
+.header .nav-right > ul li {
+  cursor: pointer;
   margin: 0 8px;
+  /* background-color: skyblue; */
 }
 
-.nav_search{
-  margin:0 auto;
+.nav-search {
+  /* margin: auto; */
+  flex-grow: 1;
+  display: flex;
+  line-height: 50px;
+}
+
+.nav-search .search-input {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 auto;
+  width: 500px;
+  height: 40px;
+  background-color: rgb(238, 240, 240);
+  border-radius: 8px;
+}
+.nav-search .search-input:hover,
+.nav-search .search-input:focus{
+  background-color: white;
+}
+
+.nav-search input {
+  border: 5px solid rgb(238, 240, 240);
+  border-radius: 5px;
+  margin:0 10px;
+  width: 410px;
+  height: 20px;
+  font-size: 14px;
+  flex-grow: 1;
+  /* border: 0; */
+  background-color: rgb(238, 240, 240);
+  /* background-color: rgb(226,223,226); */
+  outline: none;
+}
+.nav-search .search-img {
+  margin: 0 10px;
+  width: 18px;
+  height: 18px;
+}
+
+.nav-search input:focus {
+  background-color: rgb(219,219,223);
+  border:5px solid rgb(219,219,223);
 }
 </style>
